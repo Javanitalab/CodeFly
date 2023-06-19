1247a73e1d3ad5664fba714839b2950eca620f54
Javanitalab <118067995+Javanitalab@users.noreply.github.com>
2023-06-19 18:14:55 +0200

    test

--- CodeFly/Program.cs
+++ CodeFly/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -11,8 +12,15 @@ namespace CodeFly
 {
     public class Program
     {
+        static string Ip { get; set; }
+        static int Port { get; set; }
+
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            Ip = config.GetValue<string>("ip") ?? "0.0.0.0";
+            Port = config.GetValue<int?>("port") ?? 5000;
+            // var httpsPort = config.GetValue<int?>("https") ?? 5005;
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,6 +30,15 @@ namespace CodeFly
                 {
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseUrls("http://0.0.0.0:5000");
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.Limits.MaxRequestBodySize = 10000000;
+                        options.Listen(IPAddress.Any, Port, op =>
+                        {
+                            options.Limits.MaxConcurrentConnections = null;
+                            options.Limits.MaxConcurrentUpgradedConnections = null;
+                        });
+                    });
                 });
     }
 }
